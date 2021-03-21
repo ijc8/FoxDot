cdfd2166888c0d7cec266f26d59b7896e3816c78
Ian Clester <ijc@ijc8.me>
2021-03-21 12:48:52 -0400

    Remove modules outside of the subset.

--- FoxDot/lib/Custom/startup.py
+++
@@ -1,6 +0,0 @@
-# startup.py
-# ----------
-# This file is loaded on startup of FoxDot. You can edit it to include any code
-# you want to run at the beginning of a session. To use an alternate startup
-# file use the --startup flag followed by a valid path. You can also run FoxDot
-# with a --no-startup flag to not load this file.
